0aa53b22b66bb4b30904abae41bd04c0a3f9e50e
Hoseini <ssj.hosseini@ismc.ir>
2023-08-28 13:52:39 +0330

    webMVC

---
+++ WebMVC/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Services.Users.Commands.RegisterUser;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IRegisterUserService _registerUser;
+
+        public UsersController(IRegisterUserService registerUser)
+        {
+            _registerUser = registerUser;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(RequestRegisterUserDto request)
+        {
+            var result = await _registerUser.Execute(request);
+
+            return Json(result);
+        }
+    }
+}
